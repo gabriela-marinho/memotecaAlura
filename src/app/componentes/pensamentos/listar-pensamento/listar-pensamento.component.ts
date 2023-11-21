e0d3f886f39d9a91fa7ed1c5c4648b7b8874e0c4
GabrielaPaiva <gabriela@redeis.app.br>
2023-11-21 04:52:55 -0300

    FEAT pensamento componet, rotas e diretibvas

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -6,6 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./listar-pensamento.component.css']
 })
 export class ListarPensamentoComponent implements OnInit {
+  listaPensamentos = [
+    {
+      conteudo: 'passo informaçoes para componet filho',
+      autoria: 'component pai',
+      modelo:'modelo3'
+    },
+    {
+      conteudo: 'minha propriedade é decoprada com @ input',
+      autoria: 'filho',
+      modelo:'modelo3'
+    }
+  ];
 
   constructor() { }
 
